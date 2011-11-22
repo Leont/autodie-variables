150ef6ae1e0b2471a122ba55e64fa8a183cd433a
Leon Timmermans <fawaka@gmail.com>
2011-11-22 23:55:49 +0100

    Initial implementation

--- lib/autodie/variables.pm
+++ lib/autodie/variables.pm
@@ -6,4 +6,38 @@ use XSLoader;
 
 XSLoader::load(__PACKAGE__, __PACKAGE__->VERSION);
 
+_reset_global($_) for ($<, $>, $(, $));
+
+sub import {
+	$^H |= 0x020000;
+	$^H{autodie_variables} = 1;
+	return;
+}
+
+sub unimport {
+	$^H |= 0x020000;
+	$^H{autodie_variables} = 0;
+	return;
+}
+
 1;
+
+# ABSTRACT: Safe user and group ID variables
+
+__END__
+
+=head1 SYNOPSIS
+
+ use autodie::variables;
+
+ local $> = $<;
+
+=head1 DESCRIPTION
+
+This module overrides the magic on the user and group ID variables (C<< $< >>, C<< $> >>, C<$(>, C<$)>) to throw errors when assignment to them fails.
+
+=begin Pod::Coverage
+
+unimport
+
+=end Pod::Coverage
